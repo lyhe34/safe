633b4f924edeeebdd9359620d3cc78f9d921d684
lyhe34 <luc.eymard@live.fr>
2023-11-14 18:36:39 +0100

    imported bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 import { registerReactControllerComponents } from '@symfony/ux-react';
 import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,6 +9,8 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+require('bootstrap');
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
